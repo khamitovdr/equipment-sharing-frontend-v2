25799c0c737c2bee9493ad2f860b10ca03e587ad
Denis Khamitov <hamiitov.97@mail.ru>
2023-12-29 19:56:41 +0300

    Add react-query and react-query-devtools dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import MainPage from "./pages/MainPage";
 import Auth from "./components/Auth";
 
+
+const queryClient = new QueryClient()
+
 function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<Auth />
 			<MainPage />
-		</>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
