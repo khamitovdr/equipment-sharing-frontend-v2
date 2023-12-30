7526da4461b9a669420c6c4a8d7b83ad5ba46fa0
Denis Khamitov <hamiitov.97@mail.ru>
2023-12-30 21:01:13 +0300

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-// import MainPage from "./pages/MainPage";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 // import Auth from "./components/Auth";
-import PathStateExample from './pages/PathStateExample';
-
-
-const queryClient = new QueryClient()
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			{/* <Auth />
-			<MainPage /> */}
-			<PathStateExample />
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<Routes>
+			<Route path="/" element={<MainPage />} />
+		</Routes>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,25 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
 			<BrowserRouter>
-				<CssBaseline />
-				<App />
+				<QueryClientProvider client={queryClient}>
+					<CssBaseline />
+					<App />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
 			</BrowserRouter>
 		</React.StrictMode>,
 	);
