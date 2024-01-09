9a27876dd72df07c5b0674606bb60fa790783fb3
Denis Khamitov <hamiitov.97@mail.ru>
2024-01-09 21:31:22 +0300

    Refactor EquipmentCard and EquipmentList components

--- src/wigets/EquipmentCard.tsx
+++ src/wigets/EquipmentCard.tsx
@@ -1,9 +1,11 @@
 import {
 	Button,
+	ButtonProps,
 	CardActionArea,
 	CardActions,
-	Tooltip,
 	Chip,
+	Skeleton,
+	Tooltip,
 } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -11,16 +13,88 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { EquipmentPreview } from "../models/equipment";
 
-type EquipmentCardProps = {
-	equipment: EquipmentPreview;
+const cardImageHeight = "260px";
+
+const RentButton = (props: ButtonProps) => {
+	return (
+		<Button
+			size="small"
+			color="primary"
+			style={{ flexGrow: 1 }}
+			variant="contained"
+			{...props}
+		>
+			Арендовать
+		</Button>
+	);
 };
 
-const EquipmentCard = ({ equipment }: EquipmentCardProps) => {
-	const { name, photo_and_video, price, time_interval, category } = equipment;
-	const avatar = photo_and_video.length
-		? import.meta.env.VITE_API_URL + photo_and_video[0].derived_path.medium
-		: "https://via.placeholder.com/300";
+const CategoryChip = ({ name }: { name?: string }) => {
+	return (
+		<Chip
+			label={name}
+			variant="outlined"
+			size="small"
+			style={{ marginBottom: "10px" }}
+		/>
+	);
+};
 
+const EquipmentName = ({
+	children,
+}: { children: string | React.ReactNode }) => {
+	return (
+		<Typography
+			gutterBottom
+			variant="h6"
+			style={{
+				fontSize: "1rem",
+				overflow: "hidden",
+				textOverflow: "ellipsis",
+				display: "-webkit-box",
+				WebkitLineClamp: 2,
+				WebkitBoxOrient: "vertical",
+				lineHeight: "1.4em", // Adjust line height as needed
+				maxHeight: "3em", // Line height multiplied by number of lines
+			}}
+		>
+			{children}
+		</Typography>
+	);
+};
+
+type EquipmentPriceProps =
+	| {
+			isSceleton: false;
+			price: number;
+			time_interval: string;
+	  }
+	| {
+			isSceleton: true;
+	  };
+
+const EquipmentPrice = (props: EquipmentPriceProps) => {
+	const { isSceleton } = props;
+	return (
+		<Typography variant="h5" color="primary" fontSize="1.3rem">
+			{isSceleton ? (
+				<Skeleton width="40%" />
+			) : (
+				`${props.price}₽/${props.time_interval}`
+			)}
+		</Typography>
+	);
+};
+
+const CardLayout = ({
+	media,
+	children,
+	actions,
+}: {
+	media: React.ReactNode;
+	children: React.ReactNode;
+	actions: React.ReactNode;
+}) => {
 	return (
 		<Card
 			sx={{
@@ -34,54 +108,77 @@ const EquipmentCard = ({ equipment }: EquipmentCardProps) => {
 			elevation={3}
 		>
 			<CardActionArea>
-				<CardMedia
-					component="img"
-					height="260px"
-					image={avatar}
-					alt={name}
-					style={{ objectFit: "contain" }}
-				/>
-				<CardContent>
-					<Chip
-						label={category.name}
-						variant="outlined"
-						size="small"
-						style={{ marginBottom: "10px" }}
-					/>
-					<Tooltip title={name}>
-						<Typography
-							gutterBottom
-							variant="h6"
-							style={{
-								fontSize: "1rem",
-								overflow: "hidden",
-								textOverflow: "ellipsis",
-								display: "-webkit-box",
-								WebkitLineClamp: 2,
-								WebkitBoxOrient: "vertical",
-								lineHeight: "1.4em", // Adjust line height as needed
-								maxHeight: "3em", // Line height multiplied by number of lines
-							}}
-						>
-							{name}
-						</Typography>
-					</Tooltip>
-					<Typography variant="h5" color="primary" fontSize="1.3rem">
-						{price}₽/{time_interval}
-					</Typography>
-				</CardContent>
+				{media}
+				<CardContent>{children}</CardContent>
 			</CardActionArea>
 			<CardActions style={{ display: "flex", justifyContent: "center" }}>
-				<Button
-					size="small"
-					color="primary"
-					style={{ flexGrow: 1 }}
-					variant="contained"
-				>
-					Арендовать
-				</Button>
+				{actions}
 			</CardActions>
 		</Card>
+	);
+};
+
+type LoadingEquipmentCardProps = {
+	isLoading: true;
+};
+
+type LoadedEquipmentCardProps = {
+	isLoading: false;
+	equipment: EquipmentPreview;
+};
+
+type EquipmentCardProps = LoadingEquipmentCardProps | LoadedEquipmentCardProps;
+
+const EquipmentCard = (props: EquipmentCardProps) => {
+	const { isLoading } = props;
+	if (!isLoading) {
+		const { equipment } = props;
+		const { name, photo_and_video, price, time_interval, category } = equipment;
+		const avatar = photo_and_video.length
+			? import.meta.env.VITE_API_URL + photo_and_video[0].derived_path.medium
+			: "https://via.placeholder.com/300";
+
+		return (
+			<CardLayout
+				media={
+					<CardMedia
+						component="img"
+						height={cardImageHeight}
+						image={avatar}
+						alt={name}
+						style={{ objectFit: "contain" }}
+					/>
+				}
+				actions={<RentButton />}
+			>
+				<CategoryChip name={category.name} />
+				<Tooltip title={name}>
+					<EquipmentName>{name}</EquipmentName>
+				</Tooltip>
+				<EquipmentPrice
+					isSceleton={false}
+					price={price}
+					time_interval={time_interval}
+				/>
+			</CardLayout>
+		);
+	}
+
+	return (
+		<CardLayout
+			media={<Skeleton variant="rectangular" height={cardImageHeight} />}
+			actions={<RentButton disabled />}
+		>
+			<Skeleton width="70%">
+				<CategoryChip />
+			</Skeleton>
+
+			<EquipmentName>
+				<Skeleton />
+			</EquipmentName>
+
+			<EquipmentPrice isSceleton={true} />
+		</CardLayout>
 	);
 };
 
--- src/wigets/EquipmentList.tsx
+++ src/wigets/EquipmentList.tsx
@@ -1,6 +1,26 @@
 import EquipmentCard from "../wigets/EquipmentCard";
-import { Grid } from "@mui/material";
+import { Grid, GridProps } from "@mui/material";
 import { EquipmentPreview } from "../models/equipment";
+
+const GridItem = ({
+	children,
+	...props
+}: { children: React.ReactNode; props?: React.FC<GridProps> }) => {
+	return (
+		<Grid
+			{...props}
+			item
+			xs={12}
+			sm={6}
+			md={4}
+			lg={3}
+			justifyContent="center"
+			display="flex"
+		>
+			{children}
+		</Grid>
+	);
+};
 
 const EquipmentList = ({
 	equipmentList,
@@ -8,20 +28,19 @@ const EquipmentList = ({
 }: { equipmentList: EquipmentPreview[]; isPending: boolean }) => {
 	return (
 		<Grid container spacing={3} sx={{ padding: "30px 0" }}>
-			{isPending && <div>Loading...</div>}
+			{isPending &&
+				[...Array(12)].map((_, i) => (
+					<GridItem
+						// biome-ignore lint/suspicious/noArrayIndexKey: placeholder cards don't need a key
+						key={i}
+					>
+						<EquipmentCard isLoading={true} />
+					</GridItem>
+				))}
 			{equipmentList?.map((equipment) => (
-				<Grid
-					item
-					xs={12}
-					sm={6}
-					md={4}
-					lg={3}
-					key={equipment.id}
-					justifyContent="center"
-					display="flex"
-				>
-					<EquipmentCard equipment={equipment} />
-				</Grid>
+				<GridItem key={equipment.id}>
+					<EquipmentCard isLoading={false} equipment={equipment} />
+				</GridItem>
 			))}
 		</Grid>
 	);
