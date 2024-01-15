489476ea0b31f739c64a9e9fe5a9325db38782ff
Denis Khamitov <hamiitov.97@mail.ru>
2024-01-15 15:33:44 +0300

    Routes declared separetely

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import NotFoundPage from "./pages/PageNotFound";
 import PrivateRoutes from "./router/PrivateRoutes";
+import { Routes as AppRoutes } from "./router/routes";
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<MainPage />} />
+			<Route path={AppRoutes.Home} element={<MainPage />} />
 			<Route element={<PrivateRoutes />}>
-				<Route path="/my-equipment/:id" element={<MainPage />} />
+				<Route path={AppRoutes.About} element={<NotFoundPage />} />
+				<Route path={AppRoutes.Contacts} element={<NotFoundPage />} />
+				<Route path={AppRoutes.Notifications} element={<NotFoundPage />} />
 			</Route>
 			<Route path="*" element={<NotFoundPage />} />
 		</Routes>
---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+export enum Routes {
+	Home = "/",
+	About = "/about",
+    Contacts = "/contacts",
+    History = "/history",
+    Notifications = "/notifications",
+    Rentals = "/rentals",
+    Request = "/request",
+    Settings = "/settings",
+    Signup = "/signup",
+}
