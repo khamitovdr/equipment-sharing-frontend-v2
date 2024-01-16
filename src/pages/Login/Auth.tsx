275e43968c1d6084683be88b6519118c0ec9ee86
Denis Khamitov <hamiitov.97@mail.ru>
2024-01-16 11:20:19 +0300

    Refactor login form to use useRef

--- src/pages/Login/Auth.tsx
+++ src/pages/Login/Auth.tsx
@@ -1,48 +1,53 @@
 import { Box, Button, Dialog, TextField } from "@mui/material";
 import { observer } from "mobx-react-lite";
-import { useState } from "react";
-import { useContext } from "react";
+import { useRef, useContext } from "react";
 import { StoreContext } from "../../storeContext";
 
 const Auth = () => {
 	const { authStore } = useContext(StoreContext);
 	const isAuth = authStore.isAuthenicated();
 
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+	const usernameRef = useRef<HTMLInputElement>(null);
+	const passwordRef = useRef<HTMLInputElement>(null);
+
+	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const username = usernameRef.current?.value;
+		const password = passwordRef.current?.value;
+		if (!username || !password) return;
+		authStore.login({ username, password });
+	};
 
 	return (
 		<Dialog open={!isAuth}>
-			<Box p={4} display="flex" flexDirection="column" alignContent="center">
-				<TextField
-					autoFocus
-					margin="normal"
-					label="Email"
-					type="email"
-					fullWidth
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-				<TextField
-					autoFocus
-					margin="normal"
-					id="password"
-					label="Password"
-					type="password"
-					fullWidth
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<Button
-					onClick={() => {
-						authStore.login({ username, password });
-					}}
-					variant="contained"
-					sx={{ mt: 2 }}
-				>
-					Login
-				</Button>
-			</Box>
+			<form onSubmit={onSubmit}>
+				<Box p={4} display="flex" flexDirection="column" alignContent="center">
+					<TextField
+						inputRef={usernameRef}
+						autoFocus
+						margin="normal"
+						label="Email"
+						type="email"
+						fullWidth
+					/>
+					<TextField
+						inputRef={passwordRef}
+						autoFocus
+						margin="normal"
+						id="password"
+						label="Password"
+						type="password"
+						fullWidth
+					/>
+					<Button
+						type="submit"
+						variant="contained"
+						sx={{ mt: 2 }}
+					>
+						Login
+					</Button>
+				</Box>
+			</form>
 		</Dialog>
 	);
 };
