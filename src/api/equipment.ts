4c080a1fda9ea4cffaf616c44a7e457da3ef08e4
Denis Khamitov <hamiitov.97@mail.ru>
2024-01-18 12:46:48 +0300

    Refactor API client and add axios configuration

--- src/api/equipment.ts
+++ src/api/equipment.ts
@@ -1,7 +1,7 @@
-import { client } from "./client";
+import axios from "axios";
 import { EquipmentPreview } from "../models/equipment";
 
 export const fetchEquipmentList = async () => {
-	const { data } = await client.get<EquipmentPreview[]>("/equipment/");
+	const { data } = await axios.get<EquipmentPreview[]>("/equipment/");
 	return data;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { axiosConfig } from "./config.ts";
+
+axiosConfig();
 
 const queryClient = new QueryClient();
 
