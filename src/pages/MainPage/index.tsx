6492218efe2a7e929837c6ce291dcbbed58b998b
denis240997 <hamitov.97@mail.ru>
2024-06-28 23:55:55 +0300

    Slider added

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,12 +1,14 @@
 import Container from "@mui/material/Container";
 import EquipmentForRent from "src/pages/Rentals/EquipmentForRent";
+import Slider from "src/widgets/Slider";
 import MainPageHeader from "./MainPageHeader";
 
 const MainPage = () => {
 	return (
 		<>
 			<MainPageHeader />
-			<Container maxWidth="lg">
+			<Container maxWidth="lg" disableGutters>
+				<Slider />
 				<EquipmentForRent />
 			</Container>
 		</>
