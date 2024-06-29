cc5b2ecb1e10883ca9bfe86c9ac49d79519a974b
denis240997 <hamitov.97@mail.ru>
2024-06-29 23:19:20 +0300

    Undo last commit

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,14 +1,12 @@
 import Container from "@mui/material/Container";
 import EquipmentForRent from "src/pages/Rentals/EquipmentForRent";
-import Slider from "src/widgets/Slider";
 import MainPageHeader from "./MainPageHeader";
 
 const MainPage = () => {
 	return (
 		<>
 			<MainPageHeader />
-			<Container maxWidth="lg" disableGutters>
-				<Slider />
+			<Container maxWidth="lg">
 				<EquipmentForRent />
 			</Container>
 		</>
